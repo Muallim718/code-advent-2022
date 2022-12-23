444e39f1d7f62d42aba326ef9ff82db28b56b6b9
Muallim718 <100230672+Muallim718@users.noreply.github.com>
2022-12-23 03:00:03 +0000

    Completed first part

--- day1/calories.c
+++ day1/calories.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
+#define SIZE 100
+#define ELF_MAX 1000
+
+int main(int argc, char *argv[]) {
+
+    FILE *file = fopen(argv[1], "r");
+    char buffer[SIZE];
+    int calories[ELF_MAX];
+    int empty_line = 0;
+    int value = 0;
+    int number = 0;
+
+    if (argc != 2) {
+        printf("Usage: ./calories list.txt\n");
+        return 1;
+    }
     
+    if (file == NULL) {
+        printf("File failed to open.\n");
+        return 1;
+    }
+    
+    while (fgets(buffer, SIZE, file) != NULL) {
+        number = atoi(buffer);
+        value += number; 
+        if (buffer[0] == '\n') {
+            calories[empty_line] = value;
+            empty_line++;
+            value = 0;
+        }
+    }
+
+    int elf_number = empty_line;
+    int max = calories[0];
+    for (int i = 0; i < elf_number; i++) {
+        if (calories[i] > max) {
+            max = calories[i];
+        }
+    }
+
+    printf("The maximum amount of calories is %i.\n", max);
+
+    fclose(file);
+    return 0;
 }
