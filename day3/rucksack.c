3ae318062668813d3dde0cb80e715f55d669cc7f
Muallim718 <100230672+Muallim718@users.noreply.github.com>
2022-12-26 02:41:11 +0000

    Completed second part (dynamically)

--- day3/rucksack.c
+++ day3/rucksack.c
@@ -1,55 +1,82 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-#define SIZE 100
+#define BUFFER_SIZE 100
 
 int main(int argc, char *argv[]) {
 
     FILE *file = fopen(argv[1], "r");
-    char buffer[SIZE];
-    char first_half[SIZE];
-    char second_half[SIZE];
-    char copy;
-    int copy_count = 0;
-    int length = 0;
-    int priority = 0;
-    int priority_sum = 0;
-    int upper_case_difference = 26;
-    int lower_case_conversion = 96;
-    int upper_case_conversion = 64;
+    char buffer[BUFFER_SIZE], **first_elf, **second_elf, **third_elf, copy;
+    int iteration = 1, length = 0, copy_count = 0, first_elf_size = 0, second_elf_size = 0, third_elf_size = 0;
+    int priority = 0, priority_sum = 0;
+    int upper_case_difference = 26, lower_case_conversion = 96, upper_case_conversion = 64;
 
-    if (argc != 2) {
-        printf("Usage: ./rucksack rucksack.txt\n");
-        return 1;
-    }
+    first_elf = malloc(BUFFER_SIZE * sizeof(char *));
+    second_elf = malloc(BUFFER_SIZE * sizeof(char *));
+    third_elf = malloc(BUFFER_SIZE * sizeof(char *));
 
-    if (file == NULL) {
+    if (!file) {
         printf("File failed to open.\n");
         return 1;
     }
 
-    while(fgets(buffer, SIZE, file) != NULL) {
+    if (argc != 2) {
+        printf("Usage: ./test2 rucksack.txt\n");
+        return 1;
+    }
+
+    while(fgets(buffer, BUFFER_SIZE, file) != NULL) {
         length = strlen(buffer);
-        for (int i = 0; i < length / 2; i++) {
-            first_half[i] = buffer[i];
-            second_half[i] = buffer[i + length / 2];
-        }
-        for (int i = 0; i < length / 2; i++) {
-            for (int j = 0; j < length / 2; j++) {
-                if ((first_half[i] == second_half[j]) && copy_count == 0) {
-                    copy = first_half[i];
-                    copy_count++;
-                }
+        buffer[length] = '\0';
+        if (iteration == 1) {
+            first_elf_size = strlen(buffer);
+            for (int i = 0; i < length; i++) {
+                first_elf[i] = malloc(length * sizeof(char));
+                *first_elf[i] = buffer[i];
             }
         }
-        if (islower(copy)) priority = copy - lower_case_conversion;
-        else if (isupper(copy)) priority = copy - upper_case_conversion + upper_case_difference;
-        priority_sum += priority;
-        copy_count = 0;
+        else if (iteration == 2) {
+            second_elf_size = strlen(buffer);
+            for (int i = 0; i < length; i++) {
+                second_elf[i] = malloc(length * sizeof(char));
+                *second_elf[i] = buffer[i];
+            }
+        }
+        else if (iteration == 3) {
+            third_elf_size = strlen(buffer);
+            for (int i = 0; i < length; i++) {
+                third_elf[i] = malloc(length * sizeof(char));
+                *third_elf[i] = buffer[i];
+            }
+            iteration = 0;
+        }
+
+        if (iteration == 0) {
+            for (int i = 0; i < first_elf_size; i++) {
+                for (int j = 0; j < second_elf_size; j++) {
+                    for (int k = 0; k < third_elf_size; k++) {
+                        if ((*first_elf[i] == *second_elf[j]) && (*first_elf[i] == *third_elf[k]) && copy_count == 0) {
+                            copy = *first_elf[i];
+                            copy_count++;
+                        }
+                    }
+                }
+            }
+
+            if (islower(copy)) priority = copy - lower_case_conversion;
+            else if (isupper(copy)) priority = copy - upper_case_conversion + upper_case_difference;
+            priority_sum += priority;
+            copy_count = 0;
+        }
+
+        length = 0;
+        iteration++;
     }
     printf("The sum of the priorities of all items is %i!\n", priority_sum);
 
-    fclose(file);
-    return 0;
+    free(first_elf);
+    free(second_elf);
+    free(third_elf);
 }
